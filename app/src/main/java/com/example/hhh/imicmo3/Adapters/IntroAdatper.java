892d66844c02413abf5c25e7623e7d198cc8509f
MrHuu <huuphong91@gmail.com>
2018-11-08 12:46:48 +0700

    init commit

---
+++ app/src/main/java/com/example/hhh/imicmo3/Adapters/IntroAdatper.java
@@ -0,0 +1,45 @@
+package com.example.hhh.imicmo3.Adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.hhh.imicmo3.R;
+
+public class IntroAdatper  extends PagerAdapter {
+    public int[] image_resources = {R.drawable.introscreen1,R.drawable.introscreen2,R.drawable.introscreen3,R.drawable.introscreen4,R.drawable.introscreen5};
+    private Context context;
+
+    public IntroAdatper(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return image_resources.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return (view == object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        ImageView imageView = new ImageView(context);
+        imageView.setImageResource(image_resources[position]);
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        container.addView(imageView);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View)object);
+    }
+}
---
+++ app/src/main/java/com/example/hhh/imicmo3/Views/IntroActivity.java
@@ -0,0 +1,113 @@
+package com.example.hhh.imicmo3.Views;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.hhh.imicmo3.Adapters.IntroAdatper;
+import com.example.hhh.imicmo3.R;
+
+import me.relex.circleindicator.CircleIndicator;
+
+public class IntroActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private IntroAdatper customPagerAdapter;
+    private TextView tvSkip;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        SharedPreferences sharedPreferences = getSharedPreferences("check_first_login", MODE_PRIVATE);
+        boolean checkFirstVisit = sharedPreferences.getBoolean("isFirstVisit", true);
+        if (checkFirstVisit) {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("isFirstVisit", false);
+            editor.apply();
+            builtIntroViewpager();
+            setTvSkip();
+        } else {
+            translateMain();
+        }
+    }
+
+    private void translateMain() {
+        Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.anim_slide_in_right, R.anim.anim_slide_out_left);
+        finish();
+    }
+
+    private void setTvSkip() {
+        tvSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.anim_slide_in_right, R.anim.anim_slide_out_left);
+            }
+        });
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == ((customPagerAdapter.image_resources.length) - 1)) {
+                    tvSkip.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+    }
+
+    private void builtIntroViewpager() {
+        viewPager = findViewById(R.id.viewpager);
+        customPagerAdapter = new IntroAdatper(this);
+        viewPager.setAdapter(customPagerAdapter);
+        tvSkip = findViewById(R.id.skip);
+        CircleIndicator indicator = findViewById(R.id.indicator);
+        indicator.setViewPager(viewPager);
+        customPagerAdapter.registerDataSetObserver(indicator.getDataSetObserver());
+
+        CubeOutRotationTransformation cubeOutRotationTransformation = new CubeOutRotationTransformation();
+        viewPager.setPageTransformer(true, cubeOutRotationTransformation);
+    }
+
+    private class CubeOutRotationTransformation implements ViewPager.PageTransformer {
+        @Override
+        public void transformPage(@NonNull View page, float position) {
+
+            if (position < -1) {    // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                page.setAlpha(0);
+
+            } else if (position <= 0) {    // [-1,0]
+                page.setAlpha(1);
+                page.setPivotX(page.getWidth());
+                page.setRotationY(-90 * Math.abs(position));
+
+            } else if (position <= 1) {    // (0,1]
+                page.setAlpha(1);
+                page.setPivotX(0);
+                page.setRotationY(90 * Math.abs(position));
+
+            } else {    // (1,+Infinity]
+                // This page is way off-screen to the right.
+                page.setAlpha(0);
+
+            }
+        }
+    }
+}
